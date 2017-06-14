7694f968a2e1b1d7651e252a3a802c7975dcde3b
Byung Ik Han <Byung Ik Han>
2017-06-14 09:33:04 +0900

    Download in user's download folder as zip file

--- routes/downloadImage.js
+++ routes/downloadImage.js
@@ -5,33 +5,42 @@ var http = require('http');
 var fs = require('fs');
 var download = require('image-downloader');
 var zipFolder = require('zip-folder');
+var os = require('os');
 var router = express.Router();
+var async = require('async');
 
 router.post('/', function(req, res, next) {
 
   console.log(req.body.checked_Image);
   var temp = req.body.checked_Image;
+  var dir = os.userInfo().username;
+
+  var destination = '/Users/'+dir+'/Downloads/imgzip';
+  if(!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
   for(var i in temp) {
     var options = {
       url : temp[i],
-      dest : '/Users/hiasi/Desktop/Download_image/image/image'+i+'.jpg'
-    }
+      dest : '/Users/'+dir+'/Downloads/imgzip/image'+i+'.jpg'
+    };
     download.image(options).then(({i,image}) => {
       console.log('File saved to', 'image'+i+'.jpg');
     }).catch((err) => {
-      throw err
-
-    })
+      throw err;
+    });
   }
-  zipFolder('/Users/hiasi/Desktop/Download_image/image','/Users/hiasi/Desktop/Download_image/image.zip', function(err) {
-    if(err) {
-      console.log('fail');
-    }
-    else {
-        console.log('sucess');
-    }
-  });
 
+  setTimeout(function() {
+    zipFolder('/Users/'+dir+'/Downloads/imgzip','/Users/'+dir+'/Downloads/imgzip.zip', function(err) {
+      if(err) {
+        console.log('fail');
+      }
+      else {
+          console.log('sucess');
+      }
+    });
+  }, 1000);
 
   res.render('index', { title: 'imgzip'});
 });
